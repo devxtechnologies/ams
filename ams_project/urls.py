982dbd3067bf9623530ac41c4d3d88fd553b5419
Mayank Kushal <mayankkushal26@gmail.com>
2018-07-03 17:59:14 +0530

    setup dashboard

--- ams_project/urls.py
+++ ams_project/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^', include('dashboard.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

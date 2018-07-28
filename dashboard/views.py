2f4784fdb5abe03ef38d26316fe8f0fa34876b1e
Mayank Kushal <mayankkushal26@gmail.com>
2018-07-28 20:35:40 +0530

    add log view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 
 class DashboardView(TemplateView):
 	template_name = "dashboard/index.html"
+
+
+class LogView(LoginRequiredMixin, TemplateView):
+	'''
+	This is to show all the changes that have been made to the existing
+	attendance.
+	'''
+	template_name = "dashboard/log.html"
+
+	def get_context_data(self, **kwargs):
+	    context = super(LogView, self).get_context_data(**kwargs)
+
+	    return context

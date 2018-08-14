34a6081ae567c8244afe96bea5cb2bb1bdd073ec
amoghng7 <amoghsk279@gmail.com>
2018-08-14 14:10:44 +0530

    urls changed

--- ams_project/urls.py
+++ ams_project/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^attendance/', include('main.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
+    url(r'^$', RedirectView.as_view(url='/attendance/')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

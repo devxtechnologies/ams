3dae4220497245a14605da60a8ee4141fdc1a004
mayank kushal <mayankkushal26@gmail.com>
2018-08-17 10:24:57 +0000

    remove unused code

--- main/views.py
+++ main/views.py
@@ -26,7 +26,6 @@ class InitialView(LoginRequiredMixin, FormView):
     def get_form_kwargs(self):
         kwargs = super(InitialView, self).get_form_kwargs()
         kwargs.update({"user": self.request.user})
-        print(scheduler.get_jobs())
         return kwargs
 
     def post(self, request, *args, **kwargs):
@@ -70,6 +69,4 @@ class AttendanceView(SendSMSMixin, LoginRequiredMixin, View):
         for i in student:
             user = User.objects.get(username=i)
             Absentees.objects.create(user=user, attendance=attendance)
-            # msg = self.get_msg(user.first_name, teaches.subject)
-            # self.send_msg(user.phone, msg)
         return redirect(reverse_lazy("initial"))
